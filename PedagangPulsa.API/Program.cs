c3af6c0bbd4031297d0bc6fa243a618cd965bb6a
rifki ahmad sururi <rifkiahmadsururi@outlook.com>
2024-10-13 05:39:59 +0700

    add swager

--- PedagangPulsa.API/Program.cs
+++ PedagangPulsa.API/Program.cs
@@ -86,11 +86,11 @@ builder.Services.AddSingleton<MqttService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 // Membuat database secara otomatis jika belum ada
 using (var scope = app.Services.CreateScope())
